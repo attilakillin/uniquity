d030fac939d8501f7325a56af3a39f11d55faf00
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2023-06-07 11:56:04 +0200

    Initial commit - initialize Spring Boot backend application

---
+++ uniquity-backend/src/main/java/attilakillin/uniquitybackend/UniquityBackendApplication.java
@@ -0,0 +1,13 @@
+package attilakillin.uniquitybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniquityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniquityBackendApplication.class, args);
+	}
+
+}
---
+++ uniquity-backend/src/test/java/attilakillin/uniquitybackend/UniquityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package attilakillin.uniquitybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniquityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
