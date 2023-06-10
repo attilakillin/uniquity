7bd9120b820bef816900c1fe142f607c0ebb148d
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2023-06-10 12:03:02 +0200

    Comment remaining tests

--- uniquity-backend/src/test/java/attilakillin/uniquitybackend/services/LoggingServiceTests.java
+++ uniquity-backend/src/test/java/attilakillin/uniquitybackend/services/LoggingServiceTests.java
@@ -19,23 +19,29 @@ public class LoggingServiceTests {
 
     @Test
     public void logging_saves_to_repo() {
+        // Mimic normal behaviour
         when(repo.save(any())).thenAnswer(i -> i.getArgument(0));
 
+        // Call service function
         service.logClientRequest("pdf");
 
+        // Verify mock was called
         verify(repo).save(any());
     }
 
     @Test
     public void logging_saves_with_valid_extension() {
+        // When called, assert that a valid argument was passed
         when(repo.save(any())).then(i -> {
             Request param = i.getArgument(0);
             Assertions.assertEquals("pdf", param.getExtension());
             return param;
         });
 
+        // Call service function
         service.logClientRequest("pdf");
 
+        // Verify mock was called
         verify(repo).save(any());
     }
 
@@ -43,33 +49,42 @@ public class LoggingServiceTests {
     public void logging_saves_with_valid_timestamp() {
         Timestamp now = new Timestamp(System.currentTimeMillis());
 
+        // When called, assert that a valid argument was passed
         when(repo.save(any())).then(i -> {
             Request param = i.getArgument(0);
             Assertions.assertTrue(param.getTimestamp().after(now));
             return param;
         });
 
+        // Call service function
         service.logClientRequest("pdf");
 
+        // Verify mock was called
         verify(repo).save(any());
     }
 
     @Test
     public void history_returns_empty() {
+        // Mimic behaviour
         when(repo.findAll()).thenReturn(new ArrayList<>());
 
+        // Call service function
         Collection<Request> result = service.getHistoricalRequests();
 
+        // Assert returned collection size
         Assertions.assertTrue(result.isEmpty());
     }
 
     @Test
     public void history_returns_content() {
+        // Mimic behaviour with non-empty list
         when(repo.findAll()).thenReturn(List.of(
                 new Request("host", new Timestamp(System.currentTimeMillis()), "ext")));
 
+        // Call service function
         Collection<Request> result = service.getHistoricalRequests();
 
+        // Assert returned collection size
         Assertions.assertEquals(1, result.size());
     }
 }
